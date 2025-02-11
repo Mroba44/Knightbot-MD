1ccbed3ea1fd10d6a49ee52975ad0f64f97c8c77
Mroba44 <somtooba33@gmail.com>
2025-02-11 21:28:00 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "MR OBA",
+  botOwner: 'PASCAL', // Your name
+  ownerNumber: '2250566242821', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
